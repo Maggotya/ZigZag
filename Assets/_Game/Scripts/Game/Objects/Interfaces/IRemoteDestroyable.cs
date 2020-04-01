093614c4b2b6933a77a3b54775f3ffe380831e17
Anton Kerp <anton_kerp@mail.ru>
2020-04-01 10:57:21 +0700

    IRemoteDestroyable

---
+++ Assets/_Game/Scripts/Game/Objects/Interfaces/IRemoteDestroyable.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Assets._Game.Scripts.Game.Objects.Interfaces
+{
+    interface IRemoteDestroyable : IGameObjectHost
+    {
+        bool needDestroy { get; }
+        Action<IRemoteDestroyable> onBecomeDestroyable { get; set; }
+
+        void SetDestroyed();
+    }
+}
