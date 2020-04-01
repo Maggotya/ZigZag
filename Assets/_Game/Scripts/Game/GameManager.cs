a9c5aa5fa52bd9b55571387e13f62755beb6fd40
Anton Kerp <anton_kerp@mail.ru>
2020-04-01 09:41:27 +0700

    Основа для GameManager

---
+++ Assets/_Game/Scripts/Game/GameManager.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Assets._Game.Scripts.Game
+{
+    class GameManager : MonoBehaviour, IGameManager
+    {
+        #region PUBLIC_VALUES
+        public bool gamePaused { get; private set; }
+        public bool gameStarted { get; private set; }
+        #endregion // PUBLIC_VALUES
+
+        ///////////////////////////////
+
+        #region PUBLIC_METHODS
+        public void StartGame()
+        {
+            if (gameStarted)
+                return;
+
+            gameStarted = true;
+        }
+
+        public void EndGame()
+        {
+            if (gameStarted == false)
+                return;
+
+            gameStarted = false;
+        }
+
+        public void SetPause(bool status)
+        {
+            if (gamePaused == status)
+                return;
+
+            gamePaused = status;
+            Time.timeScale = status ? 0 : 1;
+        }
+
+        public void Restart()
+            => SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+
+        public void Quit()
+        {
+            Application.Quit();
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+#endif
+        }
+        #endregion // PUBLIC_METHODS
+    }
+}
---
+++ Assets/_Game/Scripts/Game/IGameManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Assets._Game.Scripts.Game
+{
+    interface IGameManager
+    {
+        bool gamePaused { get; }
+        bool gameStarted { get; }
+
+
+        void StartGame();
+        void EndGame();
+        void SetPause(bool status);
+        void Restart();
+        void Quit();
+    }
+}
