99f0cd78a9727c07adecfbb9f134a7e0c8d48b8f
Anton Kerp <anton_kerp@mail.ru>
2020-04-01 15:26:51 +0700

    View

---
+++ Assets/_Game/Scripts/UI/IView.cs
@@ -0,0 +1,10 @@
+﻿using Assets._Game.Scripts.Game.Objects.Interfaces;
+
+namespace Assets._Game.Scripts.UI
+{
+    interface IView : IActive
+    {
+        void Enable();
+        void Disable();
+    }
+}
