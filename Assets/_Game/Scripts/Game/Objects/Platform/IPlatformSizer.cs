01ff10b2511a0e0d195f15174c4463101e18679f
Anton Kerp <anton_kerp@mail.ru>
2020-04-01 10:53:46 +0700

    PlatformSizer

---
+++ Assets/_Game/Scripts/Game/Objects/Platform/IPlatformSizer.cs
@@ -0,0 +1,13 @@
+﻿
+namespace Assets._Game.Scripts.Game.Objects.Platform
+{
+    interface IPlatformSizer
+    {
+        float size { get; set; }
+        float thickness { get; set; }
+
+        void SetSize(float size);
+        void SetThickness(float thickness);
+        void Set(float size, float thickness);
+    }
+}
---
+++ Assets/_Game/Scripts/Game/Objects/Platform/PlatformSizer.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+
+namespace Assets._Game.Scripts.Game.Objects.Platform
+{
+    [RequireComponent(typeof(Transform))]
+    class PlatformSizer : MonoBehaviour, IPlatformSizer
+    {
+        #region SERIALIZE_FIELDS
+        [SerializeField] private float _Size;
+        [SerializeField] private float _Thickness;
+        #endregion // SERIALIZE_FIELDS
+
+        #region PUBLIC_VALUES
+        public float size {
+            get => _Size;
+            set {
+                _Size = value;
+                UpdateSize();
+            }
+        }
+
+        public float thickness {
+            get => _Thickness;
+            set {
+                _Thickness = value;
+                UpdateSize();
+            }
+        }
+        #endregion // PUBLIC_VALUES
+
+        ////////////////////////////////////
+
+        #region MONO_BEHAVIOUR
+        private void OnValidate()
+            => UpdateSize();
+        #endregion // MONO_BEHAVIOUR
+
+        #region PUBLIC_METHODS
+        public void SetSize(float size)
+            => this.size = size;
+        public void SetThickness(float thickness)
+            => this.thickness = thickness;
+        public void Set(float size, float thickness)
+        {
+            SetSize(size);
+            SetThickness(thickness);
+        }
+        #endregion // PUBLIC_METHODS
+
+        #region PRIVATE_METHODS
+        private void UpdateSize()
+        {
+            if (transform == null)
+                return;
+
+            transform.localScale = new Vector3(size, thickness, size);
+        }
+        #endregion // PRIVATE_METHODS
+    }
+}
