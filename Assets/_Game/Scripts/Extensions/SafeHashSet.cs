36b0663275f233644da9b08a1d54be8408bb74e6
Anton Kerp <anton_kerp@mail.ru>
2020-04-01 10:32:10 +0700

    SafeHashSet

---
+++ Assets/_Game/Scripts/Extensions/SafeHashSet.cs
@@ -0,0 +1,22 @@
+﻿
+namespace System.Collections.Generic
+{
+    class SafeHashSet<T> : HashSet<T>
+    {
+        public new void Add(T value)
+        {
+            if (Contains(value))
+                return;
+
+            base.Add(value);
+        }
+
+        public new void Remove(T value)
+        {
+            if (Contains(value) == false)
+                return;
+
+            base.Remove(value);
+        }
+    }
+}
