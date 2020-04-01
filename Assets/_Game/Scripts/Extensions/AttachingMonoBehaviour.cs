6aaf64dc290202aefa03824619233ebb3ad7dec6
Anton Kerp <anton_kerp@mail.ru>
2020-04-01 10:25:59 +0700

    AttachingMonoBehaviour

---
+++ Assets/_Game/Scripts/Extensions/AttachingMonoBehaviour.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace UnityEngine
+{
+    class AttachingMonoBehaviour : MonoBehaviour
+    {
+        #region MONO_BEHAVIOUR
+        private void Awake()
+        {
+            OnAttaching();
+            OnAwakeActions();
+        }
+        #endregion // MONO_BEHAVIOUR
+
+        #region PROTECTED_METHODS
+        protected bool Attach<T>(ref T component) where T : UnityEngine.Object
+            => component = component ??  GetComponent<T>();
+        #endregion // PROTECTED_METHODS
+
+        #region METHODS_TO_OVERRIDE
+        protected virtual void OnAwakeActions()
+        { }
+
+        protected virtual void OnAttaching()
+        { }
+        #endregion // METHODS_TO_OVERRIDE
+    }
+}
