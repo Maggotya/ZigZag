2bd8adb0d806f494245145f9ce43e5968816f205
Anton Kerp <anton_kerp@mail.ru>
2020-04-01 13:17:01 +0700

    Probabilitier

---
+++ Assets/_Game/Scripts/Game/Handlers/Probability/IProbabilitier.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets._Game.Scripts.Game.Handlers.Probability
+{
+    interface IProbabilitier
+    {
+        float successProbability { get; }
+        bool IsSuccess();
+    }
+}
---
+++ Assets/_Game/Scripts/Game/Handlers/Probability/Probabilitier.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Assets._Game.Scripts.Game.Handlers.Probability
+{
+    class Probabilitier : IProbabilitier
+    {
+        public float successProbability {get; private set;}
+
+        public Probabilitier(float successProbability)
+            => this.successProbability = successProbability;
+
+        public bool IsSuccess()
+            => RandomValue() < successProbability;
+
+        private float RandomValue()
+            => Random.Range(0f, 100f);
+    }
+}
