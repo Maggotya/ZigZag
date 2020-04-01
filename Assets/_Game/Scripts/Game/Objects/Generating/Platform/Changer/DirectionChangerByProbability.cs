8eecf7033c351af72cd2764cb5cd50a01a583ae2
Anton Kerp <anton_kerp@mail.ru>
2020-04-01 13:17:33 +0700

    DirectionChanger

---
+++ Assets/_Game/Scripts/Game/Objects/Generating/Platform/Changer/DirectionChangerByProbability.cs
@@ -0,0 +1,42 @@
+﻿using Assets._Game.Scripts.Game.Handlers.Direction;
+using Assets._Game.Scripts.Game.Handlers.Probability;
+using UnityEngine;
+using Zenject;
+
+namespace Assets._Game.Scripts.Game.Objects.Generating.Platform.Changer
+{
+    class DirectionChangerByProbability : IDirectionChanger
+    {
+        #region PRIVATE_VALUES
+        private IDirectionHandler _directionHandler { get; set; }
+        private IProbabilitier _probabilitier { get; set; }
+        #endregion // PRIVATE_VALUES
+
+        #region PUBLIC_VALUES
+        public Vector3 currentDirection => _directionHandler.current;
+        #endregion // PUBLIC_VALUES
+
+        ///////////////////////////////////////////
+
+        #region CONSTRUCTORS
+        [Inject]
+        public DirectionChangerByProbability(IDirectionHandler directionHandler, IProbabilitier probabilitier)
+        {
+            _directionHandler = directionHandler;
+            _probabilitier = probabilitier;
+        }
+        #endregion // CONSTRUCTORS
+
+        //////////////////////////////////////////
+
+        #region PUBLIC_METHODS
+        public Vector3 ChangeDirection()
+        {
+            if (_probabilitier.IsSuccess())
+                _directionHandler.MoveNext();
+
+            return _directionHandler.current;
+        }
+        #endregion // PUBLIC_METHODS
+    }
+}
---
+++ Assets/_Game/Scripts/Game/Objects/Generating/Platform/Changer/IDirectionChanger.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace Assets._Game.Scripts.Game.Objects.Generating.Platform.Changer
+{
+    interface IDirectionChanger
+    {
+        Vector3 currentDirection { get; }
+
+        Vector3 ChangeDirection();
+    }
+}
