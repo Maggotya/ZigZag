ee76b0ae8ce876dcb5cd4f3ba514ae8d70ce245e
Anton Kerp <anton_kerp@mail.ru>
2020-04-01 09:55:01 +0700

    GamePrefabs

---
+++ Assets/_Game/Scripts/Parameters/Classes/GamePrefabs.cs
@@ -0,0 +1,21 @@
+﻿using Assets._Game.Scripts.Parameters.Interfaces;
+using UnityEngine;
+
+namespace Assets._Game.Scripts.Parameters.Classes
+{
+    [CreateAssetMenu(fileName = "GamePrefabs", menuName = "ScriptableObjects/GamePrefabs")]
+    class GamePrefabs : ScriptableObject, IGamePrefabs
+    {
+        #region SERIALIZE_FIELDS
+        [SerializeField] private GameObject _Ball;
+        [SerializeField] private GameObject _Gem;
+        [SerializeField] private GameObject _Platform;
+        #endregion // SERIALIZE_FIELDS
+
+        #region PUBLIC_VALUES
+        public GameObject ball => _Ball;
+        public GameObject gem => _Gem;
+        public GameObject platform => _Platform;
+        #endregion // PUBLIC_VALUES
+    }
+}
---
+++ Assets/_Game/Scripts/Parameters/Interfaces/IGamePrefabs.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace Assets._Game.Scripts.Parameters.Interfaces
+{
+    interface IGamePrefabs
+    {
+        GameObject ball { get; }
+        GameObject gem { get; }
+        GameObject platform { get; }
+    }
+}
