ec2010ee8dbb66a0ee8239b1bad7e811bc2f2e7c
Anton Kerp <anton_kerp@mail.ru>
2020-04-01 14:14:20 +0700

    GemProducer

---
+++ Assets/_Game/Scripts/Game/Objects/Generating/Gem/Producer/IGemProducer.cs
@@ -0,0 +1,9 @@
+﻿using Assets._Game.Scripts.Game.Objects.Gem;
+using Assets._Game.Scripts.Game.Objects.Interfaces;
+
+namespace Assets._Game.Scripts.Game.Objects.Generating.Gem.Producer
+{
+    interface IGemProducer : IProducer<IGem>
+    {
+    }
+}
---
+++ Assets/_Game/Scripts/Game/Objects/Generating/Gem/Producer/PooledGemProducer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Assets._Game.Scripts.Game.Objects.Gem;
+using Assets._Game.Scripts.Game.Objects.Generating.Gem.Factory;
+using UnityEngine;
+
+namespace Assets._Game.Scripts.Game.Objects.Generating.Gem.Producer
+{
+    class PooledGemProducer : IGemProducer
+    {
+        public PooledGemProducer(Transform container, int poolSize, IGemFactory factory)
+            => throw new NotImplementedException();
+
+        public void HandleUnused(IGem gem)
+            => throw new NotImplementedException();
+        public void HandleUnused(IGem[] gems)
+            => throw new NotImplementedException();
+
+        public IGem Produce()
+            => throw new NotImplementedException();
+
+        private void FiilPool()
+            => throw new NotImplementedException();
+    }
+}
