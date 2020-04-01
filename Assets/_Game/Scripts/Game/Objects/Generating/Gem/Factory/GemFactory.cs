18960b2cd7b6488f217866884c4d20d73182ff21
Anton Kerp <anton_kerp@mail.ru>
2020-04-01 14:13:25 +0700

    GemFactory

---
+++ Assets/_Game/Scripts/Game/Objects/Generating/Gem/Factory/GemFactory.cs
@@ -0,0 +1,10 @@
+﻿using Assets._Game.Scripts.Game.Objects.Gem;
+using Zenject;
+
+namespace Assets._Game.Scripts.Game.Objects.Generating.Gem.Factory
+{
+    class GemFactory : PlaceholderFactory<IGem>, IGemFactory
+    {
+        
+    }
+}
---
+++ Assets/_Game/Scripts/Game/Objects/Generating/Gem/Factory/IGemFactory.cs
@@ -0,0 +1,10 @@
+﻿using Assets._Game.Scripts.Game.Objects.Gem;
+using Assets._Game.Scripts.Game.Objects.Interfaces;
+
+namespace Assets._Game.Scripts.Game.Objects.Generating.Gem.Factory
+{
+    interface IGemFactory : IFactory<IGem>
+    {
+
+    }
+}
