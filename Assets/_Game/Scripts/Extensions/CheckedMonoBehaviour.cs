ecd7f536f9ac13bf2e1fe311facfcd83f9fa034e
Anton Kerp <anton_kerp@mail.ru>
2020-04-01 10:26:17 +0700

    CheckedMonoBehaviour

---
+++ Assets/_Game/Scripts/Extensions/CheckedMonoBehaviour.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Assets;
+
+namespace UnityEngine
+{
+    class CheckedMonoBehaviour : MonoBehaviour
+    {
+        #region MONO_BEHAVIOUR
+        private void Awake()
+        {
+            OnCheckFields();
+            OnAwakeActions();
+        }
+        #endregion // MONO_BEHAVIOUR
+
+        #region PROTECTED_METHODS
+        protected bool CheckField<T>(T component) where T : UnityEngine.Object
+        {
+            if (component.Equals(null)) {
+                throw new NullReferenceException($"{GetType().Name}: {typeof(T)} isn't attached!");
+            }
+            return true;
+        }
+        #endregion // PROTECTED_METHODS
+
+        #region METHODS_TO_OVERRIDE
+        protected virtual void OnAwakeActions()
+        { }
+
+        protected virtual void OnCheckFields()
+        { }
+        #endregion // METHODS_TO_OVERRIDE
+    }
+}
---
+++ Assets/_Game/Scripts/Extensions/IntExtensions.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+    static class IntExtensions
+    {
+        public static bool IsInInterval(this int value, int minBorder, int maxBorder)
+            => minBorder <= value && value <= maxBorder;
+    }
