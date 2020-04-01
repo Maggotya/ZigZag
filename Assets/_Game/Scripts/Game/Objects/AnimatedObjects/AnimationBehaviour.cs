901c19bc96be57b1581573a18af2ef158531bf65
Anton Kerp <anton_kerp@mail.ru>
2020-04-01 10:58:42 +0700

    AnimationBehaviour

---
+++ Assets/_Game/Scripts/Game/Objects/AnimatedObjects/AnimationBehaviour.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+namespace Assets._Game.Scripts.Game.Objects.AnimatedObjects
+{
+    public class AnimationBehaviour : StateMachineBehaviour
+    {
+        // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+        override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            if (animator.TryGetComponent<IAnimated>(out var animated))
+                animated.OnAnimationStart(stateInfo);
+        }
+
+        // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+        //override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        //{
+        //    
+        //}
+
+        // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+        override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            if (animator.TryGetComponent<IAnimated>(out var animated))
+                animated.OnAnimationEnd(stateInfo);
+        }
+
+        // OnStateMove is called right after Animator.OnAnimatorMove()
+        //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        //{
+        //    // Implement code that processes and affects root motion
+        //}
+
+        // OnStateIK is called right after Animator.OnAnimatorIK()
+        //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        //{
+        //    // Implement code that sets up animation IK (inverse kinematics)
+        //}
+    }
+}
