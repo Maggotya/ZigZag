0637fe9668e0f57f5f30ec4ebccaf127a3a107cb
Anton Kerp <anton_kerp@mail.ru>
2020-04-01 14:38:18 +0700

    Integration with GameManager

--- Assets/_Game/Scripts/Game/GameManager.cs
+++ Assets/_Game/Scripts/Game/GameManager.cs
@@ -1,16 +1,39 @@
-﻿using UnityEngine;
+﻿using Assets._Game.Scripts.Game.Objects.Ball;
+using Assets._Game.Scripts.Game.Objects.Generating.Gem;
+using Assets._Game.Scripts.Game.Objects.Generating.Platform;
+using Assets._Game.Scripts.Game.Save;
+using Assets._Game.Scripts.Game.Scoring;
+using UnityEngine;
 using UnityEngine.SceneManagement;
+using Zenject;
 
 namespace Assets._Game.Scripts.Game
 {
     class GameManager : MonoBehaviour, IGameManager
     {
+        #region PRIVATE_VALUES
+        [Inject] private IBall _ball { get; set; }
+        [Inject] private IPlatformGenerator _platformGenerator { get; set; }
+        [Inject] private IGemGenerator _gemGenerator { get; set; }
+
+        [Inject] private IScore _score { get; set; }
+        [Inject] private ISaveManager _saveManager { get; set; }
+        #endregion // PRIVATE_VALUES
+
         #region PUBLIC_VALUES
         public bool gamePaused { get; private set; }
         public bool gameStarted { get; private set; }
         #endregion // PUBLIC_VALUES
 
-        ///////////////////////////////
+        #region CONSTS
+        private const int PAUSED_SCALE = 0;
+        private const int UNPAUSED_SCALE = 1;
+        #endregion // CONSTS
+
+        #region MONO_BEHAVIOUR
+        private void Start()
+            => Initialize();
+        #endregion // MONO_BEHAVIOUR
 
         #region PUBLIC_METHODS
         public void StartGame()
@@ -19,6 +42,9 @@ namespace Assets._Game.Scripts.Game
                 return;
 
             gameStarted = true;
+
+            _platformGenerator.SetActive(true);
+            _ball.StartMove();
         }
 
         public void EndGame()
@@ -27,6 +53,10 @@ namespace Assets._Game.Scripts.Game
                 return;
 
             gameStarted = false;
+            SaveGame();
+
+            _platformGenerator.SetActive(false);
+            _ball.StopMove();
         }
 
         public void SetPause(bool status)
@@ -35,7 +65,7 @@ namespace Assets._Game.Scripts.Game
                 return;
 
             gamePaused = status;
-            Time.timeScale = status ? 0 : 1;
+            Time.timeScale = status ? PAUSED_SCALE : UNPAUSED_SCALE;
         }
 
         public void Restart()
@@ -49,5 +79,22 @@ namespace Assets._Game.Scripts.Game
 #endif
         }
         #endregion // PUBLIC_METHODS
+
+        #region PRIVATE_METHODS
+        private void Initialize()
+        {
+            gameStarted = false;
+            gamePaused = false;
+            Time.timeScale = UNPAUSED_SCALE;
+
+            _platformGenerator.GenerateInitialPlatforms();
+            _ball.SetPositionOnSurface(_platformGenerator.GetInitialPositionOn());
+        }
+        private void SaveGame()
+        {
+            if (_saveManager.lastSave.bestScore < _score.score)
+                _saveManager.Save(_score.score);
+        }
+        #endregion // PRIVATE_METHODS
     }
 }
---
+++ Assets/_Game/Scripts/Installers/SceneObjectsInstaller.cs
@@ -0,0 +1,27 @@
+﻿using Assets._Game.Scripts.Game.Objects.Ball;
+using Assets._Game.Scripts.Game.Objects.Generating.Gem;
+using Assets._Game.Scripts.Game.Objects.Generating.Platform;
+using Assets._Game.Scripts.Game.Scoring;
+using Assets._Game.Scripts.UI.Manager;
+using Cinemachine;
+using UnityEngine;
+using Zenject;
+
+namespace Assets._Game.Scripts.Installers
+{
+    class SceneObjectsInstaller : MonoInstaller
+    {
+        [SerializeField] private GameObject _Ball;
+        [SerializeField] private GameObject _PlatformGenerator;
+        [SerializeField] private GameObject _GemGenerator;
+        [SerializeField] private GameObject _Score;
+
+        public override void InstallBindings()
+        {
+            Container.Bind<IGemGenerator>().To<GemGenerator>().FromComponentOn(_GemGenerator).AsSingle();
+            Container.Bind<IPlatformGenerator>().To<PlatformGenerator>().FromComponentOn(_PlatformGenerator).AsSingle();
+            Container.Bind<IBall>().To<BallObject>().FromComponentOn(_Ball).AsSingle();
+            Container.Bind<IScore>().To<Score>().FromComponentOn(_Score).AsSingle();
+        }
+    }
+}
